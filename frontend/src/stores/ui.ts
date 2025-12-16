2f4039f304da8fc1d600e19d3d39f75d510242f3
luna system <luna@airsi.de>
2025-12-16 03:19:51 -0600

    Deploy Sphinx documentation to GitHub Pages

--- frontend/src/stores/ui.ts
+++
@@ -1,6 +0,0 @@
-import { writable } from 'svelte/store';
-
-export type Panel = 'none' | 'debug' | 'mem' | 'prompt' | 'conversations' | 'notices';
-
-export const panelOpen = writable<Panel>('none');
-export const menuOpen = writable<boolean>(false);
